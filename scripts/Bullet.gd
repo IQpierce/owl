50db0f9bcf6c6f264025143a585b6bf1a33610dc
Shay Pierce <IQpierce@gmail.com>
2024-01-11 14:17:55 -0700

    more iteration post-stream. also fixed a bug where bullets were imparting

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -7,7 +7,7 @@ func on_body_entered(other_body:Node):
 	
 	var other_body_rigid:RigidBody2D = other_body as RigidBody2D
 	if (other_body_rigid):
-		other_body_rigid.apply_impulse(linear_velocity.normalized() * impact_force, global_position)
+		other_body_rigid.apply_impulse(linear_velocity.normalized() * impact_force, global_position - other_body.global_position)
 		
 		var other_body_thing:Thing = other_body_rigid as Thing
 		if (other_body_thing):
