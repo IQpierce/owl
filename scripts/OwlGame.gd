57325f23a26ceaf75ad16d17eebba9fab86f1ee1
Sam Trapp <>
2024-01-11 20:54:25 -0700

    moving escape-to-quit over to OwlGame so it is always accessible

--- scripts/OwlGame.gd
+++ scripts/OwlGame.gd
@@ -5,6 +5,10 @@ extends Node2D
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	Engine.max_fps = 60
+	
+func _process(delta):
+	if Input.is_action_pressed("escape"):
+		get_tree().quit()
 
 func reset_score():
 	current_score = 0
