79cd755d02fd8f54efdf1ca67485dc4d67cc2b3c
tryptefan <tryptefan@gmail.com>
2024-01-21 11:03:30 -0600

    Hum
    Added the background hum

--- scripts/PhosphorEmulation.gd
+++ scripts/PhosphorEmulation.gd
@@ -4,3 +4,8 @@ class_name PhosphorEmulation
 
 @export var viewport_container:SubViewportContainer
 @export var viewport:SubViewport
+
+@onready var hum = $SubViewportContainer/SubViewport/CanvasLayer/ColorRect/Hum
+
+func _ready():
+	hum.play()
