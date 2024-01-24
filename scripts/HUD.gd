eeff253feeadd43b5acb543d2923894dc8a2c02a
Shay Pierce <IQpierce@gmail.com>
2024-01-24 13:18:21 -0700

    Added time-speed-up cheat

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @export var game:OwlGame
 @export var game_over_layer:CanvasLayer
 @export var debug_fps_label:Label
+@export var debug_time_speed_up_label:Label
 @export var post_game_over_input_cooldown_secs:float = 2
 
 var accept_input_timeout:float
@@ -21,10 +22,18 @@ func _process(delta):
 		if game.game_over && Input.is_anything_pressed():
 			get_tree().reload_current_scene()
 		
-		# @TODO - This part is debug/cheat functionality...
+		# @TODO - From here on is debug/cheat functionality...
 		# ...we should compile it out for "real" builds!
 		if Input.is_action_just_pressed("debug_toggle_fps_display"):
 			debug_fps_label.visible = !debug_fps_label.visible
+		
+		if Input.is_action_pressed("debug_time_speed_up"):
+			debug_time_speed_up_label.visible = true
+			Engine.time_scale = 5
+		else:
+			debug_time_speed_up_label.visible = false
+			Engine.time_scale = 1
+
 	
 
 func on_game_over(won:bool):
