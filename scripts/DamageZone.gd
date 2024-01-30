59ea3645b2a0786f36f428413e541e5bc5f68809
Shay Pierce <IQpierce@gmail.com>
2024-01-30 13:45:47 -0700

    Planetoid now has an inner damage zone

---
+++ scripts/DamageZone.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+class_name DamageZone
+
+@export var damage_per_sec:float = 1
+@export var damage_cooldown_secs:float = 2
+
+signal on_damaged_thing(damaged_thing:Thing, damage_amt:float)
+signal on_damaged_body(damaged_body:RigidBody2D, damage_amt:float)
+
+var things_to_damage:Array[Thing]
+
+func _ready():
+	self.body_entered.connect(_on_body_entered)
+	self.body_exited.connect(_on_body_entered)
+
+func _process(delta:float):
+	var damage_amount:float = delta * damage_per_sec
+	
+	for thing_to_damage:Thing in things_to_damage:
+		if thing_to_damage == null:
+			continue
+		
+		# @TODO: Choose a better position? What if its center-point is not inside of our area...? That could look weird!
+		thing_to_damage.deal_damage(damage_amount, thing_to_damage.global_position)
+
+func _on_body_entered(body:RigidBody2D):
+	var body_thing:Thing = body as Thing
+	if body_thing:
+		things_to_damage.append(body_thing)
+
+func _on_body_exited(body:RigidBody2D):
+	var body_thing:Thing = body as Thing
+	if body_thing:
+		things_to_damage.erase(body_thing)
