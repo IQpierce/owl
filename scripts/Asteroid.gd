24575a8ce7c9ba0211278929dba497f09e06f673
Sam Trapp <>
2024-02-01 08:12:08 -0700

    preventing the get_supports error spam by disallowing asteroids from colliding with their layer for their first couple frames after being spawned.

--- scripts/Asteroid.gd
+++ scripts/Asteroid.gd
@@ -5,6 +5,16 @@ class_name Asteroid
 # Keys are ResourceType's, values are relevance for seeker brains.
 @export var resource_container_nibbleable_relevance:Dictionary
 
+func _ready():
+	super()
+	# Sam 1-31-24: When we spawn mini-asteroids, they sometimes spawn on top of each other, which cause physics system to
+	# fall over and spam errors about get_supports. Disallowing collisions on their own layer for a couple frames appears
+	# to prevent the issue; probably an initialization order thing.
+	var real_collision_mask = collision_mask
+	collision_mask = collision_mask & ~collision_layer
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	collision_mask = real_collision_mask
 
 func spawns_resource_containers_when_nibbled(resource_type:GameEnums.ResourceType) -> float:
 	if resource_container_nibbleable_relevance.has(resource_type):
