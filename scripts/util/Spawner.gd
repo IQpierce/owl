1f19adf42d5e1971205dceaa1f710f514bdfa410
Shay Pierce <IQpierce@gmail.com>
2024-02-18 14:27:23 -0700

    Spawners can now fake-spawn other things (to reuse the same direction/velocity)

--- scripts/util/Spawner.gd
+++ scripts/util/Spawner.gd
@@ -17,34 +17,36 @@ class_name Spawner
 @export var min_initial_velocity:float = 40.0
 @export var max_initial_velocity:float = 80
 
+func handle_body_as_spawned(spawning_body:Node2D):
+	var spawn_angle_degrees:float = randf_range(-spawn_random_angle*0.5, spawn_random_angle*0.5)
+	var spawn_dist:float = randf_range(min_spawn_dist, max_spawn_dist)
+	
+	var spawn_velocity_direction:Vector2 = (global_position - global_transform.origin)
+	
+	# add our random degrees/angle
+	spawn_velocity_direction = Vector2.RIGHT.rotated(spawn_velocity_direction.angle() + deg_to_rad(spawn_angle_degrees))
+	
+	spawning_body.global_position = global_position + (spawn_velocity_direction * spawn_dist)
+	
+	# initialize velocity, if possible
+	var spawning_body_as_rigidbody:RigidBody2D = spawning_body as RigidBody2D
+	if spawning_body_as_rigidbody != null:
+		var spawn_velocity_power:float = randf_range(min_initial_velocity, max_initial_velocity)
+		spawning_body_as_rigidbody.set_axis_velocity(spawn_velocity_direction * spawn_velocity_power)
+	
+
 func spawn(spawn_owner:Node2D):
 	assert(spawn_owner != null)	
 	
 	# Wait until we know we are done with collision, otherwise we get warnings on every spawn
 	await get_tree().process_frame
-
+	
 	for i in num_to_spawn:
 		var spawn_proto:PackedScene = spawn_protos[randi() % spawn_protos.size()]
 		var spawned_instance:CollisionObject2D = spawn_proto.instantiate()
 		spawn_owner.add_child(spawned_instance)
 		
-		var spawn_angle_degrees:float = randf_range(-spawn_random_angle*0.5, spawn_random_angle*0.5)
-		var spawn_dist:float = randf_range(min_spawn_dist, max_spawn_dist)
-		
-		var spawn_velocity_direction:Vector2 = (global_position - global_transform.origin)
-		
-		# add our random degrees/angle
-		spawn_velocity_direction = Vector2.RIGHT.rotated(spawn_velocity_direction.angle() + deg_to_rad(spawn_angle_degrees))
-		
-		spawned_instance.global_position = global_position + (spawn_velocity_direction * spawn_dist)
-		
-		# initialize velocity, if possible
-		var spawned_instance_as_rigidbody:RigidBody2D = spawned_instance as RigidBody2D
-		if spawned_instance_as_rigidbody:
-			var spawn_velocity_power:float = randf_range(min_initial_velocity, max_initial_velocity)
-			spawned_instance_as_rigidbody.set_axis_velocity(spawn_velocity_direction * spawn_velocity_power)
-
-		if free_after_spawn:
-			queue_free
-
+		handle_body_as_spawned(spawned_instance)
 	
+	if free_after_spawn:
+		queue_free
