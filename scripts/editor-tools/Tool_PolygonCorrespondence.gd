3ef0fcba893a281c4cf3ca1cbec660a26c59d21b
Sam Trapp <>
2024-03-11 12:00:41 -0600

    Keep PolygonCorresponce always positive and flip vertex signs if needed. Negative scales are not consistent due to matrix math (no way to separate negative rotation from negative scale).

--- scripts/editor-tools/Tool_PolygonCorrespondence.gd
+++ scripts/editor-tools/Tool_PolygonCorrespondence.gd
@@ -13,8 +13,8 @@ class_name PolygonCorrespondence
 func _ready():
 	assert(counterpart != null)
 	counterpart.matching_collider = self
-	if !Engine.is_editor_hint() && runtime_sync_onready:
-		sync_polygon_data()
+	#if !Engine.is_editor_hint() && runtime_sync_onready:
+	#	sync_polygon_data()
 
 func _process(delta:float):
 	
@@ -33,3 +33,17 @@ func sync_polygon_data():
 	self.global_rotation = counterpart.global_rotation
 	self.global_scale = counterpart.global_scale
 	self.polygon = counterpart.polygon if match_patchwork else counterpart.raw_polygon
+	#print(get_parent().name, global_scale, counterpart.global_scale)
+
+	# Setting a negative global_scale is inconsistent (sometimes reverting to all positive),
+	# so lets always keep global_scale positive here and just flip vertices to compensate
+	var self_global_scale = counterpart.global_scale
+	if self_global_scale.x < 0 || self_global_scale.y < 0:
+		var x_mul = -1 if self_global_scale.x < 0 else 1
+		var y_mul = -1 if self_global_scale.y < 0 else 1
+		var mirror_polygon = PackedVector2Array()
+		mirror_polygon.resize(self.polygon.size())
+		for i in self.polygon.size():
+			mirror_polygon[i] = Vector2(self.polygon[i].x * x_mul, self.polygon[i].y * y_mul)
+		self.polygon = mirror_polygon
+		self.global_scale = Vector2(abs(self_global_scale.x), abs(self_global_scale.y))
