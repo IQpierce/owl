c69acee3f681d9212b5bc47bc40dcba37a88af6e
Sam Trapp <>
2024-03-25 09:42:40 -0600

    If locomotor is trying to turn, prevent RigidBody physics from stopping it. We might want an override at some point, but for now we just need to be able to get of walls and corners.

--- scripts/Locomotor.gd
+++ scripts/Locomotor.gd
@@ -32,28 +32,36 @@ static var stop_below_speed: = 15
 # might want to look into the  _integrate_forces() callback
 @export var side_boost:float
 
-var body:Thing
+var _body:Thing
 var camera:Camera2D
 var driving:bool
 var turning_left:bool
 var turning_right:bool
 var at_max_speed:bool
+var _body_lock_rotation:bool
+
+var body:Thing:
+	get:
+		return _body
+	set(value):
+		_body = value
+		_body_lock_rotation = _body.lock_rotation
 
 signal driving_state_change(enabled:bool)
 signal turning_left_state_change(enabled:bool)
 signal turning_right_state_change(enabled:bool)
 
 func _compute_turn(turn_towards_global:Vector2, turn_fraction:float, delta:float) -> float:
-	if body == null:
+	if _body == null:
 		return 0.0
 
 	var turn_factor = 0
 	turn_fraction = max(turn_fraction, 0)
-	var to_turn_towards = (turn_towards_global - body.global_position)
+	var to_turn_towards = (turn_towards_global - _body.global_position)
 	if to_turn_towards.length_squared() > 0:
 		to_turn_towards = to_turn_towards.normalized()
-		var up = Vector2.UP.rotated(body.global_rotation)
-		var right = Vector2.RIGHT.rotated(body.global_rotation)
+		var up = Vector2.UP.rotated(_body.global_rotation)
+		var right = Vector2.RIGHT.rotated(_body.global_rotation)
 		var axis = 1
 		if right.dot(to_turn_towards) < 0:
 			axis = -1
@@ -65,12 +73,12 @@ func _compute_turn(turn_towards_global:Vector2, turn_fraction:float, delta:float
 	return turn_factor * turn_fraction
 
 func turn(turn_factor:float, turn_drift:float, delta:float):
-	if body == null:
+	if _body == null:
 		return
 
 	#var turn_drift = 1.0
 
-	var heading_dir:Vector2 = Vector2.UP.rotated(body.global_rotation)
+	var heading_dir:Vector2 = Vector2.UP.rotated(_body.global_rotation)
 
 	if turn_factor < 0:
 		if !turning_left:
@@ -88,34 +96,40 @@ func turn(turn_factor:float, turn_drift:float, delta:float):
 			turning_right_state_change.emit(false)
 			turning_right = false
 
-	if heading_dir.rotated(body.angular_velocity * delta).dot(body.linear_velocity) >= heading_dir.dot(body.linear_velocity):
+	# TODO (sam) Are we cool checking exactly Zero here or do we need is_equal_approx
+	if turn_factor != 0:
+		_body.lock_rotation = true
+	else:
+		_body.lock_rotation = _body_lock_rotation
+
+	if heading_dir.rotated(_body.angular_velocity * delta).dot(_body.linear_velocity) >= heading_dir.dot(_body.linear_velocity):
 		turn_drift *= turn_with_velocity_turn_factor;
 
 	# Godot Phyics applies damp as val *= 1.0 - step * damp (where step is essentially delta)
 	# We can combine that damp with ours to approximate a true max_turn_speed and the artificially reduce that
-	var approx_ang_damp = (1 - (delta * body.angular_damp)) * turn_drift
-	var new_turn_speed = body.angular_velocity + turn_force * turn_factor * delta
-	if (body.angular_velocity > 0 && turn_factor < 0) || (body.angular_velocity < 0 && turn_factor > 0):
+	var approx_ang_damp = (1 - (delta * _body.angular_damp)) * turn_drift
+	var new_turn_speed = _body.angular_velocity + turn_force * turn_factor * delta
+	if (_body.angular_velocity > 0 && turn_factor < 0) || (_body.angular_velocity < 0 && turn_factor > 0):
 		# Allow some finesse when changing direction to avoid ping-ponging
-		body.angular_velocity *= 0.5
+		_body.angular_velocity *= 0.5
 		turn_factor *= 0.5
 
-	if abs(body.angular_velocity) < 0.001:
-		body.angular_velocity = 0
+	if abs(_body.angular_velocity) < 0.001:
+		_body.angular_velocity = 0
 
-	if abs(new_turn_speed * approx_ang_damp * turn_factor) > abs(body.angular_velocity):
-		body.angular_velocity = new_turn_speed
-	body.angular_velocity *= turn_drift
+	if abs(new_turn_speed * approx_ang_damp * turn_factor) > abs(_body.angular_velocity):
+		_body.angular_velocity = new_turn_speed
+	_body.angular_velocity *= turn_drift
 	#if turn_factor == 0:
-	#	body.angular_velocity *= 0.75
-	#print(body.angular_velocity, " | ", approx_ang_damp)
+	#	_body.angular_velocity *= 0.75
+	#print(_body.angular_velocity, " | ", approx_ang_damp)
 
 func turn_towards(turn_towards_global:Vector2, turn_fraction:float, delta:float):
 	var turn_factor = _compute_turn(turn_towards_global, turn_fraction, delta)
 	turn(turn_factor, 1, delta)
 
 func drive(drive_factor:float, drive_drift:float, delta:float, relative_direction:Vector2 = Vector2.UP):
-	if body == null:
+	if _body == null:
 		return
 
 	var time_now = Time.get_ticks_msec()
@@ -124,13 +138,13 @@ func drive(drive_factor:float, drive_drift:float, delta:float, relative_directio
 	var max_speed_factor = 1.0
 	var acceleration_factor = drive_factor
 
-	var heading_dir:Vector2 = relative_direction.normalized().rotated(body.global_rotation)
+	var heading_dir:Vector2 = relative_direction.normalized().rotated(_body.global_rotation)
 
 	var cap_speed = max_speed * max_speed_factor * anti_zoom
 	at_max_speed = false
 
 	if drive_factor > 0:
-		var speed_portion = clamp((body.linear_velocity.length() / max_speed) * heading_dir.dot(body.linear_velocity.normalized()), 0, 1)
+		var speed_portion = clamp((_body.linear_velocity.length() / max_speed) * heading_dir.dot(_body.linear_velocity.normalized()), 0, 1)
 		var band_portion = 1.0
 		if speed_portion < peak_band_min:
 			band_portion = speed_portion / peak_band_min
@@ -140,14 +154,14 @@ func drive(drive_factor:float, drive_drift:float, delta:float, relative_directio
 		var drive_delta = drive_force * drive_lerp * delta;
 		var acceleration = heading_dir * drive_delta;
 
-		if body.linear_velocity.length_squared() > 0:
-			var velocity_dir = body.linear_velocity.normalized()
+		if _body.linear_velocity.length_squared() > 0:
+			var velocity_dir = _body.linear_velocity.normalized()
 			var head_para_vel:Vector2 = heading_dir.project(velocity_dir)
 			var head_perp_vel:Vector2 = heading_dir - head_para_vel
 
 			var drive_para_vel = head_para_vel * drive_delta
-			var potential_velocity:Vector2 = body.linear_velocity + drive_para_vel
-			if body.linear_velocity.length_squared() > (cap_speed * cap_speed):
+			var potential_velocity:Vector2 = _body.linear_velocity + drive_para_vel
+			if _body.linear_velocity.length_squared() > (cap_speed * cap_speed):
 				head_para_vel = Vector2.ZERO
 			elif potential_velocity.length_squared() > (cap_speed * cap_speed) && head_para_vel.dot(velocity_dir) > 0:
 				var excess = potential_velocity - (velocity_dir * cap_speed)
@@ -158,11 +172,11 @@ func drive(drive_factor:float, drive_drift:float, delta:float, relative_directio
 
 		acceleration *= acceleration_factor
 
-		body.apply_central_impulse(acceleration * drive_delta * anti_zoom)
+		_body.apply_central_impulse(acceleration * drive_delta * anti_zoom)
 
 		var hard_speed_cap = hard_speed_limit * anti_zoom
-		if !ignore_speed_limit && body.linear_velocity.length_squared() > hard_speed_cap * hard_speed_cap:
-			body.linear_velocity = body.linear_velocity * hard_speed_cap
+		if !ignore_speed_limit && _body.linear_velocity.length_squared() > hard_speed_cap * hard_speed_cap:
+			_body.linear_velocity = _body.linear_velocity * hard_speed_cap
 
 		if (!driving):
 			driving = true
@@ -172,16 +186,16 @@ func drive(drive_factor:float, drive_drift:float, delta:float, relative_directio
 			driving = false
 			driving_state_change.emit(false)
 
-	if body.linear_velocity.length_squared() > (cap_speed * cap_speed):
+	if _body.linear_velocity.length_squared() > (cap_speed * cap_speed):
 		drive_drift *= excessive_speed_linear_damp_factor;
 
 	if drive_drift <= 0.99: # Cannot trust 1 to mean 1 every frame apparantly; maybe it is the rigidbody damping
-		body.linear_velocity *= drive_drift
+		_body.linear_velocity *= drive_drift
 
 	# TODO (sam) need to force velocity to zero if near it, because there are some calculations that behave subtely different 
 	var min_speed = stop_below_speed * anti_zoom
-	if !driving && body.linear_velocity.length_squared() < min_speed * min_speed:
-		body.linear_velocity = Vector2.ZERO
+	if !driving && _body.linear_velocity.length_squared() < min_speed * min_speed:
+		_body.linear_velocity = Vector2.ZERO
 
 func locomote(drive_factor:float, turn_factor:float, delta:float, relative_direction:Vector2 = Vector2.UP):
 	var drive_drift = 1.0
