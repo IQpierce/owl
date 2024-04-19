216f7834f8c88488b816f598d08a65ad71ba087e
Sam Trapp <>
2024-04-19 16:54:38 -0600

    Allowing seekers to seek their old target's last-seen position and update that position every frame. Drawing debug lines to show where seeker is trying to go. Still not quite happy with mosquito, but getting there.

--- scripts/creatures/brains/SeekerBrain.gd
+++ scripts/creatures/brains/SeekerBrain.gd
@@ -2,6 +2,10 @@ extends Brain
 
 class_name SeekerBrain
 
+var last_seeked_target:CollisionObject2D = null
+var last_seeked_position:Vector2 = global_position
+
+@export var debug_seek:bool = false
 
 signal on_target_body_changed(body:CollisionObject2D)
 
@@ -15,25 +19,48 @@ signal on_seek_failure()
 #  relevance never changes after we cache it in this dictionary!!!
 var relevant_targets:Dictionary		
 
+func _process(delta:float):
+	if debug_seek:
+		queue_redraw()
+	#print("      ", name, last_seeked_target)
+
 func think():
 	process_seek_result(seek())
 	
-func process_seek_result(most_relevant_target:CollisionObject2D):
+func process_seek_result(most_relevant_target:CollisionObject2D, quick_process = false):
+	var seek_pos = last_seeked_position
+	#print(name, ": ", most_relevant_target, " | ", last_seeked_target, " | ", last_seeked_position)
+	if most_relevant_target == null:
+		if last_seeked_target != null:
+			# TODO (sam) Considering dt here would be nice but we don't have access to it.
+			var seek_dist_sqr = (seek_pos - controlled_creature.global_position).length_squared()
+			if seek_dist_sqr > controlled_creature.linear_velocity.length_squared() / 10:
+				most_relevant_target = last_seeked_target
+			else:
+				last_seeked_target = null
+	else:
+		seek_pos = most_relevant_target.global_position
+		last_seeked_target = most_relevant_target
+		last_seeked_position = last_seeked_target.global_position
+
 	if most_relevant_target != null:
-		on_target_body_changed.emit(most_relevant_target)
-		on_target_location_changed.emit(most_relevant_target.global_position)
+		if true:#!quick_process:
+			on_target_body_changed.emit(most_relevant_target)
+
+		on_target_location_changed.emit(seek_pos)
 		
-		var relative_velocity_to_target:Vector2 = most_relevant_target.global_position - controlled_creature.global_position
+		var relative_velocity_to_target:Vector2 = seek_pos - controlled_creature.global_position
 
 		# TODO (sam) I really wish I could get the actual positioning I'm turning towards
 		on_target_velocity_changed.emit(relative_velocity_to_target)
 	else:
-		on_seek_failure.emit()
-		
-		# @TODO: Should these lines still exist? They should really be consequences of failure
-		on_target_body_changed.emit(null)
-		on_target_location_changed.emit(controlled_creature.global_position)
-		on_target_velocity_changed.emit(Vector2.ZERO)
+		if !quick_process:
+			on_seek_failure.emit()
+			
+			# @TODO: Should these lines still exist? They should really be consequences of failure
+			on_target_body_changed.emit(null)
+			on_target_location_changed.emit(controlled_creature.global_position)
+			on_target_velocity_changed.emit(Vector2.ZERO)
 
 # @returns the most relevant target, if any relevant targets exist at all.
 func seek() -> CollisionObject2D:
@@ -48,15 +75,15 @@ func seek() -> CollisionObject2D:
 	
 	if most_relevant_targets != null && most_relevant_targets.size() > 0:
 		var nearest_dist:float = NAN
-		var nearest_relevant_target:CollisionObject2D
+		var nearest_relevant_target:CollisionObject2D = null
 		for relevant_target in most_relevant_targets:
 			assert(relevant_target != null)
 			var dist:float = (relevant_target.global_position - controlled_creature.global_position).length_squared()
-			if is_nan(nearest_dist) || dist < nearest_dist:
+			if (is_nan(nearest_dist) || dist < nearest_dist) && is_body_detectable(relevant_target):
 				nearest_dist = dist
 				nearest_relevant_target = relevant_target
 		
-		assert(nearest_relevant_target != null)
+		#assert(nearest_relevant_target != null)
 		
 		return nearest_relevant_target
 	
@@ -64,6 +91,16 @@ func seek() -> CollisionObject2D:
 
 func is_body_relevant(body:CollisionObject2D):
 	return get_body_relevance(body) > 0
+
+func is_body_detectable(body:CollisionObject2D) -> bool:
+	if body != null:
+		var space_state = get_world_2d().direct_space_state
+		var raycast = PhysicsRayQueryParameters2D.create(controlled_creature.global_position, body.global_position)
+		var hit = space_state.intersect_ray(raycast)
+		# TODO (sam) We might need a way to check if the hit is a "child" body of the body we want
+		if hit.has("collider"):
+			return hit["collider"] == body
+	return false
 
 func on_body_enters_awareness(body:CollisionObject2D):
 	if body == controlled_creature:
@@ -109,4 +146,9 @@ func get_body_relevance(body:CollisionObject2D) -> float:
 	assert(false, "pure virtual function - must override!")
 	return 0
 	
-
+func _draw():
+	if debug_seek:
+		if last_seeked_target != null:
+			draw_line(Vector2.ZERO, to_local(last_seeked_position), OwlGame.instance.draw_point_color)
+			draw_dashed_line(to_local(last_seeked_position), to_local(last_seeked_target.global_position), OwlGame.instance.draw_point_color)
+		
--- scripts/creatures/brains/SeekerFallbackBrain.gd
+++ scripts/creatures/brains/SeekerFallbackBrain.gd
@@ -5,28 +5,44 @@ class_name SeekerFallbackBrain
 # The brain allows for "prioritization" of seeking. If the first seeker_brain 
 # fails to find anything, we will fall back to the second; etc.
 
+@export var sub_think_seek = false
+
 # If a member of this list is a SeekerBrain, seek() will be called on it.
 #	Only the last item is allowed to be a non-Seeker-brain, as a final fallback.
 @export var prioritized_brains:Array[Brain]
 
+var thought = false
+
+func _process(delta:float):
+	thought = false
+	super._process(delta)
+	if !thought && sub_think_seek:
+		var quick_seeked = last_seeked_target
+		if quick_seeked != null && !is_body_detectable(quick_seeked):
+			quick_seeked = null
+		process_seek_result(quick_seeked, true)
 
 func _ready():
 	assert(prioritized_brains.size() > 0)
 
 func think():
+	thought = true
 	for brain in prioritized_brains:
 		var seeker_brain:SeekerBrain = brain as SeekerBrain
 		if seeker_brain:
 		
 			var best_target:PhysicsBody2D = seeker_brain.seek()
 			
-			if best_target != null:
+			if best_target != null || seeker_brain.last_seeked_target != null:
 				process_seek_result(best_target)
+				last_seeked_target = seeker_brain.last_seeked_target
+				if best_target != null:
+					last_seeked_target = best_target
 				
 				# If we found any target, no need to continue executing.
 				#	We only need the fallbacks to process if the earlier cases failed.
 				return
-		else:
+		elif last_seeked_target == null:
 			# We only allow a non-seeker brain as the final in the list of fallbacks.
 			assert(prioritized_brains[prioritized_brains.size()-1] == brain)
 			brain.think()
